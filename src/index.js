6de3102be42520b604301cd200c5e19d70ea689b
vijayxtreme <vijaymenonx@gmail.com>
2020-03-03 23:49:04 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function log(Class) {
+  return (...args) => {
+    console.log(args);
+    return new Class(...args);
+  };
+}
+
+@log
+class Example {
+  constructor(name, age) {
+  }
+}
+
+const e = new Example('Graham', 34);
+// [ 'Graham', 34 ]
+console.log(e);
+// Example {}
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    module: {
+        rules: [
+          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "HTML Demo"
+        })
+    ]
+}
